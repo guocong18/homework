1bd2bf7370348ed9f16df34907da2de8c97bf85e
guocong18 <155941285+guocong18@users.noreply.github.com>
2024-06-09 17:02:09 +0800

    1

--- Final2.0/home.js
+++ Final2.0/home.js
@@ -1,3 +1,15 @@
 /* inspiration from
 https://cz.pinterest.com/pin/830703093796717544/
 */
+
+const bodyElement = document.body;
+window.addEventListener("scroll", function () {
+  const scrollTop = window.scrollY; // Vertical scroll position
+  console.log(scrollTop);
+
+  if (scrollTop > 49) {
+    bodyElement.classList.add("header-fixed");
+  } else {
+    bodyElement.classList.remove("header-fixed");
+  }
+});
